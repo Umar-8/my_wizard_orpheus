9e7b5a43efdc34fbb03a1a21ccd88213e252632e
Umar-8 <umar.6736@beaconite.edu.pk>
2024-06-22 14:58:22 +0000

    fixed issue where the bot won't respond as i created a new bot action instead of adding to it

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ myGame.createUserAction({
   parameters: ['Message from user to game'],
   howBotShouldHandle: 'Respond to the user'
 })
+
 document.getElementById('input').addEventListener('keyup', function(e) {
   if (e.code == 'Enter') { // if the user presses enter
     let userInput = document.getElementById('input').value
@@ -16,21 +17,13 @@ document.getElementById('input').addEventListener('keyup', function(e) {
     document.getElementById('input').value = ''
   }
 })
-myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
-  // Add the bot's response to the conversation
-  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
-})
 myGame.variable('score', 'Current score. Changes (positive and negatively) as the user does things.', 0)
 
 myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
-document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
+  // Add the bot's response to the conversation
+  document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
 
 document.getElementById('score').innerHTML = data.currentVariables.score.value
 })
-myGame.variable('scaredLevel', 'How scared the user is. This changes quickly. From 0 (not scared) to 50 (very scared).', 0)
 
-myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
 
-  document.body.style.backgroundColor = `rgba(0, 0, 0, ${data.currentVariables.scaredLevel.value / 50})`
-
-})
